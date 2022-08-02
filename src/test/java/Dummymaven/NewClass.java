9ae3f67e4d7626c681b4513f7911c86db82fe82c
RajGokul <04gokulrajmech@gmail.com>
2022-08-03 03:14:07 +0530

    fsgsg

---
+++ src/test/java/Dummymaven/NewClass.java
@@ -0,0 +1,11 @@
+package Dummymaven;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		System.out.println("treee");
+		System.out.println("pleasureee");
+		System.out.println("pleasureesse");
+	}
+
+}
